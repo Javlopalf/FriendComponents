919475f856d05f77abfe5368936d4402f6b6979e
Javier <ivanartcor@gmail.com>
2024-12-02 14:30:43 +0100

    a
    a

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent {
-
+  categorias = [
+    { nombre: 'Programas', imagen: '../../assets/images/programas.jpg' },
+    { nombre: 'SSD', imagen: '../../assets/images/ssd.jpg' },
+    { nombre: 'Placas Bases', imagen: '../../assets/images/placasBases.jpg' },
+    { nombre: 'Ventiladores', imagen: '../../assets/images/ventilador.jpg' },
+    { nombre: 'Procesadores', imagen: '../../assets/images/procesador.jpg' },
+    { nombre: 'Móviles', imagen: '../../assets/images/movil.jpg' },
+    { nombre: 'Memoria RAM', imagen: '../../assets/images/ram.jpg' },
+    { nombre: 'Refrigeración líquida', imagen: '../../assets/images/refri.jpg' },
+    { nombre: 'Fuentes de alimentación', imagen: '../../assets/images/fuente.jpg' },
+    { nombre: 'Torres', imagen: '../../assets/images/torre.jpg' },
+    { nombre: 'Monitores', imagen: '../../assets/images/monitor.jpg' },
+    { nombre: 'Foros', imagen: '../../assets/images/foros.jpg' }
+  ];
 }
