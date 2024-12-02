d76e116a06be74f1e1169c4d1d1661c0aa20835a
Javier López Alfaro <114283735+Javlopalf@users.noreply.github.com>
2024-12-02 13:42:04 +0100

    trabajo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ProgramasComponent } from './programas/programas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'carrito', component: CarritoComponent},
+  {path: 'programas', component: ProgramasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProgramasComponent } from './programas/programas.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    InicioComponent,
+    ProgramasComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
