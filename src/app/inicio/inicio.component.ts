7022cdcf6ed69a42da7577095e494cb9f40849ed
Ivanartcor <javierruiz.24@campuscamara.es>
2024-12-16 13:36:05 +0100

    a
    a

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -9,18 +9,18 @@ import { InicioService } from '../inicio/inicio.service';
 })
 export class InicioComponent {
   categorias = [
-    { nombre: 'Programas', imagen: '../../assets/images/programas.jpg' },
-    { nombre: 'SSD', imagen: '../../assets/images/ssd.jpg' },
-    { nombre: 'Placas Bases', imagen: '../../assets/images/placasBases.jpg' },
-    { nombre: 'Ventiladores', imagen: '../../assets/images/ventilador.jpg' },
-    { nombre: 'Procesadores', imagen: '../../assets/images/procesador.jpg' },
-    { nombre: 'Móviles', imagen: '../../assets/images/movil.jpg' },
-    { nombre: 'Memoria RAM', imagen: '../../assets/images/ram.jpg' },
-    { nombre: 'Refrigeración líquida', imagen: '../../assets/images/refri.jpg' },
-    { nombre: 'Fuentes de alimentación', imagen: '../../assets/images/fuente.jpg' },
-    { nombre: 'Torres', imagen: '../../assets/images/torre.jpg' },
-    { nombre: 'Monitores', imagen: '../../assets/images/monitor.jpg' },
-    { nombre: 'Foros', imagen: '../../assets/images/foros.jpg' }
+    { nombre: 'Programas', imagen: '../../assets/images/programas.jpg', fuente:"" },
+    { nombre: 'SSD', imagen: '../../assets/images/ssd.jpg',  fuente:"" },
+    { nombre: 'Placas Bases', imagen: '../../assets/images/placasBases.jpg', fuente:"" },
+    { nombre: 'Ventiladores', imagen: '../../assets/images/ventilador.jpg' , fuente:""},
+    { nombre: 'Procesadores', imagen: '../../assets/images/procesador.jpg', fuente:"" },
+    { nombre: 'Móviles', imagen: '../../assets/images/movil.jpg', fuente:"" },
+    { nombre: 'Memoria RAM', imagen: '../../assets/images/ram.jpg', fuente:"" },
+    { nombre: 'Refrigeración líquida', imagen: '../../assets/images/refri.jpg', fuente:"" },
+    { nombre: 'Fuentes de alimentación', imagen: '../../assets/images/fuente.jpg', fuente:"" },
+    { nombre: 'Torres', imagen: '../../assets/images/torre.jpg' , fuente:""},
+    { nombre: 'Monitores', imagen: '../../assets/images/monitor.jpg', fuente:"" },
+    { nombre: 'Foros', imagen: '../../assets/images/foros.jpg', fuente:"" }
   ];
 
   constructor(private router: Router, private inicioService: InicioService) {}
