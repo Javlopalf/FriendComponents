dc9671b0ad66bddbc85fc6be246a58dd108d0615
Javlopalf <lopezalfarojavier@gmail.com>
2024-12-17 17:58:10 +0100

    Cambios de imagenes

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -12,13 +12,13 @@ export class InicioComponent {
     { nombre: 'Programas', imagen: '../../assets/images/windowsP.jpg', fuente:"https://pixabay.com/es/" },
     { nombre: 'SSD', imagen: '../../assets/images/discoP.jpg',  fuente:"https://www.freepik.es/" },
     { nombre: 'Placas Bases', imagen: '../../assets/images/placabaseP.jpg', fuente:"https://www.freepik.es/"},
-    { nombre: 'Ventiladores', imagen: '../../assets/images/ventiladorP.jpg' , fuente:"https://pixabay.com/es/"},
+    { nombre: 'Ventiladores', imagen: '../../assets/images/ventilador2P.jpg' , fuente:"https://pixabay.com/es/"},
     { nombre: 'Procesadores', imagen: '../../assets/images/procesadorP.jpg', fuente:"https://www.freepik.es/" },
     { nombre: 'Móviles', imagen: '../../assets/images/movilesP.jpg', fuente:"https://www.freepik.es/" },
     { nombre: 'Memoria RAM', imagen: '../../assets/images/ramP.jpg', fuente:"https://www.freepik.es/" },
     { nombre: 'Refrigeración líquida', imagen: '../../assets/images/refrigeracionP.jpg', fuente:"https://www.freepik.es/" },
     { nombre: 'Fuentes de alimentación', imagen: '../../assets/images/fuente.jpg', fuente:"https://www.freepik.es/" },
-    { nombre: 'Torres', imagen: '../../assets/images/torreP.jpg' , fuente:"https://www.freepik.es/"},
+    { nombre: 'Torres', imagen: '../../assets/images/torre17.jpg' , fuente:"https://www.freepik.es/"},
     { nombre: 'Monitores', imagen: '../../assets/images/monitor.jpg', fuente:"https://pixabay.com/es/" },
     { nombre: 'Foros', imagen: '../../assets/images/foro.jpg', fuente:"https://www.freepik.es/" }
   ];
